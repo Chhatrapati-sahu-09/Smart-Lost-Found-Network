58bba31ee1149bf0e36b2c9c1ebabda8d6978ec7
Chhatrapati sahu <chhatrapatisahu09@gmail.com>
2026-03-22 14:07:16 +0530

    feat(server): add matchFound flag to item schema

--- server/models/Item.js
+++ server/models/Item.js
@@ -26,6 +26,10 @@ const itemSchema = new mongoose.Schema({
     type: String,
     default: "open",
   },
+  matchFound: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Item", itemSchema);
